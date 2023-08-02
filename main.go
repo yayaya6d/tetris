5167d5b9dfb407c990305433db27cb99c2d5c5c6
Jeff Tang <yayaya6d16@gmail.com>
2023-08-03 00:12:59 +0800

    add some test code to main

--- main.go
+++ main.go
@@ -3,16 +3,45 @@ package main
 import (
 	"fmt"
 
-	"github.com/yayaya6d/tetris/control"
+	"github.com/yayaya6d/tetris/console"
 )
 
 func main() {
-	fmt.Print("hello tetris")
+	fmt.Println("hello tetris")
 
-	var c = make(chan int)
+	// var c = make(chan int)
 
-	km := control.NewKeyBoardEventController()
-	km.StartPollingControlEvent(c)
+	// km := control.NewKeyBoardEventController()
+	// km.StartPollingControlEvent(c)
 
-	<-c
+	// <-c
+
+	// cells := [][]draw.Cell{
+	// 	{
+	// 		{
+	// 			Color:   draw.Blue,
+	// 			Content: draw.HorizontalLine,
+	// 		},
+	// 		{
+	// 			Color:   draw.Yellow,
+	// 			Content: draw.VerticalLine,
+	// 		},
+	// 	},
+	// 	{
+	// 		{
+	// 			Color:   draw.Red,
+	// 			Content: draw.HorizontalLine,
+	// 		},
+	// 		{
+	// 			Color:   draw.Green,
+	// 			Content: draw.VerticalLine,
+	// 		},
+	// 	},
+	// }
+
+	// dr := draw.NewTetrisGameDrawer()
+	// dr.Draw(cells)
+
+	g := console.NewTetrisGameConsole()
+	g.StartGame()
 }
